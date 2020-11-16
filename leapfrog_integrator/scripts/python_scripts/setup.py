018f2d0b6f6bfcd5c5c37403db9e38b6d21282f5
Jaime-Andres-Alvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2020-11-16 21:56:06 +1100

    Removed unnecessary .py files

--- leapfrog_integrator/scripts/python_scripts/setup.py
+++
@@ -1,7 +0,0 @@
-import distutils.core
-import Cython.Build
-distutils.core.setup(
-    ext_modules=Cython.Build.cythonize(["example.pyx",
-                                        "util.pyx",
-                                        "functions.pyx",
-                                        "canon_units.pyx"]))
